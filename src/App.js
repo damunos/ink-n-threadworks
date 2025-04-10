ab0ad3ffadf7b5f8973aadaa8c6fce9c565b1661
damunos <damunos@gmail.com>
2025-04-10 03:21:09 -0400

    Maybe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import ProductDetailPage from "./pages/Products/ProductDetailPage";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import GroupedProductsPage from "./pages/Products/GroupedProductsPage";
-import Design from "./pages/Design";
-import Admin from "./pages/Admin";
-import About from "./pages/About";
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products/Products';
+import ProductDetailPage from './pages/Products/ProductDetailPage';
+import DesignTool from './pages/DesignTool';
+import AdminPanel from './pages/AdminPanel';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
+  const routes = [
+    { path: '/', element: <Home /> },
+    { path: '/products', element: <Products /> },
+    { path: '/products/:styleNumber', element: <ProductDetailPage /> },
+    { path: '/design', element: <DesignTool /> },
+    { path: '/admin', element: <AdminPanel /> },
+  ];
+
+  const element = useRoutes(routes);
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="products" element={<GroupedProductsPage />} />
-        <Route path="design" element={<Design />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="about" element={<About />} />
-        <Route path="product/:style" element={<ProductDetailPage />} />
-      </Route>
-    </Routes>
+    <div className="App">
+      <Header />
+      {element}
+      <Footer />
+    </div>
   );
 }
 
